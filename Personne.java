c64e2ac0d18a9d66fcb51309e06a9023e8144d88
sebdylan <sebastien.givenchy@hotmail.fr>
2023-04-13 15:45:09 +0200

    Add files via upload

---
+++ Personne.java
@@ -0,0 +1,29 @@
+package fr.test.java;
+
+import java.util.Date;
+
+public class Personne {
+	private String nom;
+	private String prenom;
+	private Date dateNaissance;
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}	
+	
+}
